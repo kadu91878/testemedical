f31d181ad467a40207b3a06cc485e9d5586c3e5b
kadu91878 <kadu_91878@hotmail.com>
2023-09-15 14:58:00 -0300

    feat: Initial commit

---
+++ src/test/java/com/testemedical/testemedical/TestemedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testemedical.testemedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestemedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
